26bc4d1212fad0626f1be107e0a3b95dcc94a6ff
Griffin McCormick <mccormickgriffin@gmail.com>
2023-12-09 20:21:21 -0500

    added checking for image file argument

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import boto3
 import os
+import sys
 from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    # Load environment variables from .env file
-    load_dotenv()
-
+def main():
     aws_profile = os.getenv('AWS_PROFILE')
     session = boto3.Session(profile_name=aws_profile)
     s3 = session.resource('s3')
     for bucket in s3.buckets.all():
         print(bucket.name)
+
+if __name__ == "__main__":
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Verify image path argument is provided
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <image_path>")
+        sys.exit(1)
+
+    image_path = sys.argv[1]
+    if not os.path.isfile(image_path):
+        print("provided image path is not a file")
+        sys.exit(1)
+    
+    main()
+    
